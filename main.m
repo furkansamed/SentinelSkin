e4d9828e69970ca0072d9755f1877d164c3588e3
fatih çetin <138293317+fatihcetinagu@users.noreply.github.com>
2023-07-01 14:10:46 +0300

    Add files via upload

---
+++ main.m
@@ -0,0 +1,56 @@
+% test_benign
+% test_benign
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('test_benign_features done!');
+
+% test_malignant
+% test_malignant
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('test_malignant_features done!');
+
+% train_benign
+% train_benign
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('train_benign_features done!');
+
+% train_malignant
+% train_malignant
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+segmentImages(imageFolderPath, saveFolderPath);
+disp('segmentetation done!');
+
+
+imageFolderPath = fullfile('path');
+saveFolderPath = fullfile('path');
+excelFileName = 'name.xlsx';
+feature_extraction(imageFolderPath, saveFolderPath, excelFileName);
+disp('train_malignant_features done!');
+disp('All operations succesfuly completed');
